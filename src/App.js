ee0ade784c28417aa2d302a8c1d4f1b5d7bf383e
Davor Jordacevic <davor.jordacevic@fincore.com>
2020-08-17 20:24:06 +0200

    v3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       file: null,
       selectedFile: null,
       preview: "hidden",
-      hide: ""
+      hide: "",
+      data: ""
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -19,9 +20,11 @@ class App extends Component {
       file: URL.createObjectURL(event.target.files[0]),
       selectedFile: event.target.files[0],
       preview: "",
-      hide: "hidden"
+      hide: "hidden",
+      data: ""
     })
     console.log(event.target.files[0].name);
+    //this.fileUploadHandler();
   }
 
   fileUploadHandler = () => {
@@ -29,9 +32,18 @@ class App extends Component {
     const formData = new FormData();
     formData.append('image', this.state.selectedFile, this.state.selectedFile.name);
     console.log(formData);
-    axios.post('localhost:5000/identification', formData)
+    axios.post('http://localhost:5000/identification', formData)
       .then(res => {
+        const items = [];
+        Object.keys(res.data).forEach(attribute => {
+          console.log(res.data[attribute]);
+          items.push(<div key={attribute}>{attribute} => {res.data[attribute]}</div>);
+        });
+        this.setState({
+          data: items
+        })
         console.log(res);
+        console.log(res.data);
       })
   }
   render() {
@@ -40,7 +52,7 @@ class App extends Component {
       <div className="App">
         <h2>Face Recognition</h2>
         <p className="lead">No Plugins <b>Just AI</b></p>
-        <form id="file-upload-form" className="uploader" onSubmit={this.fileUploadHandler}>
+        <form id="file-upload-form" className="uploader" >
           <input id="file-upload" type="file" name="fileUpload" onChange={this.handleChange} accept="image/*" />
           <label htmlFor="file-upload" id="file-drag">
             <img id="file-image" src={this.state.file} alt="Preview" className={this.state.preview} />
@@ -50,13 +62,16 @@ class App extends Component {
               <div id="notimage" className="hidden">Please select an image</div>
               <span id="file-upload-btn" className="btn btn-primary">Select a file</span>
             </div>
-          </label>  
-          <div id="response"className={this.state.preview}>
-              <div id="messages">Treba izvuci tekst nekako</div>
-           </div>
+          </label>
         </form>
+        <div className="uploader">
+          <div id="response" className={this.state.preview}>
+            <div id="messages">{this.state.data}</div>
+            <button className="btn btn-primary" onClick={this.fileUploadHandler}>Check</button>
+          </div>
+        </div>
         <div className="footer">
-          <p>By Davor Jordacevic  ©</p>
+          <p>By Davor Jordacevic ©</p>
         </div>
       </div>
     );
